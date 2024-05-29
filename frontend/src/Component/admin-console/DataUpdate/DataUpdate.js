a0f715604ffb1c136e507eeb63ced9e2384757ed
GokilavaniPraveen <169182114+Gokilavanipraveen@users.noreply.github.com>
2024-05-29 19:48:48 +0530

    Update DataUpdate.js

--- frontend/src/Component/admin-console/DataUpdate/DataUpdate.js
+++ frontend/src/Component/admin-console/DataUpdate/DataUpdate.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
-import { Link} from 'react-router-dom';
-import './DataUpdate.css';
+import { Link } from "react-router-dom";
+import "./DataUpdate.css";
 
 function DataUpdate() {
   const [products, setProducts] = useState([]);
@@ -14,28 +14,35 @@ function DataUpdate() {
   const [price, setPrice] = useState();
   const [quantity, setQuantity] = useState();
 
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    saveData();
-  });
-
+    if (!loading) {
+      saveData();
+    }
+  }, [loading]);
   const saveData = async () => {
-    const response = await Axios.get("http://localhost:3003/products");
-    setProducts(response.data);
+    try {
+      const response = await Axios.get("http://localhost:3003/products");
+      setProducts(response.data);
+      setLoading(true);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    } finally {
+      setLoading(false);
+    }
   };
 
- 
   const AddProducts = () => {
-    Axios
-      .post("http://localhost:3003/products", {
-        id,
-        category,
-        image,
-        title,
-        subtitle,
-        description,
-        price,
-        quantity,
-      })
+    Axios.post("http://localhost:3003/products", {
+      id,
+      category,
+      image,
+      title,
+      subtitle,
+      description,
+      price,
+      quantity,
+    })
       .then(() => {
         setId("");
         setCatagory("");
@@ -50,137 +57,170 @@ function DataUpdate() {
 
     setTimeout(() => {
       saveData();
-    }, 500)
+    }, 500);
   };
 
   const deleteProduct = (id) => {
-    Axios.delete(`http://localhost:3003/products/${id}`)
+    Axios.delete(`http://localhost:3003/products/${id}`);
 
     setTimeout(() => {
       saveData();
-    }, 500)
-  }
+    }, 500);
+  };
 
-//   const [updateState, setUpdateState] = useState(-1);
-//   function handleEdit(id) {
-//     setUpdateState(id);
-//   }
-// function handleUpdate(e){
-//   e.preventDefault();
-//   setUpdateState(-1)
-// }
+  //   const [updateState, setUpdateState] = useState(-1);
+  //   function handleEdit(id) {
+  //     setUpdateState(id);
+  //   }
+  // function handleUpdate(e){
+  //   e.preventDefault();
+  //   setUpdateState(-1)
+  // }
 
-
-  return (<>
-    <div className="updateForm container">
-      <h2>New Record</h2>
-      <div className="rows">
-        <input
-          type="text"
-          placeholder="Insert Id"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-        /></div> <div className="rows">
-        <input
-          type="text"
-          placeholder="Insert category"
-          value={category}
-          onChange={(e) => setCatagory(e.target.value)}
-        />    </div> <div className="rows">
-        <input
-          type="text"
-          placeholder="Insert image"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />    </div> <div className="rows">
-        <input
-          type="text"
-          placeholder="Insert title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        /></div> <div className="rows">
-        <input
-          type="text"
-          placeholder="Insert subtitle"
-          value={subtitle}
-          onChange={(e) => setSubtitle(e.target.value)}
-        /></div> <div className="rows">
-        <input
-          type="text"
-          placeholder="Insert description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
+  return (
+    <>
+      <div className="updateForm container">
+        <h2>New Record</h2>
+        <div className="rows">
           <input
-          type="text"
-          placeholder="Insert price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
-        />
+            type="text"
+            placeholder="Insert Id"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        </div>{" "}
+        <div className="rows">
           <input
-          type="text"
-          placeholder="Insert quantity"
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        />
+            type="text"
+            placeholder="Insert category"
+            value={category}
+            onChange={(e) => setCatagory(e.target.value)}
+          />{" "}
+        </div>{" "}
+        <div className="rows">
+          <input
+            type="text"
+            placeholder="Insert image"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />{" "}
+        </div>{" "}
+        <div className="rows">
+          <input
+            type="text"
+            placeholder="Insert title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>{" "}
+        <div className="rows">
+          <input
+            type="text"
+            placeholder="Insert subtitle"
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
+          />
+        </div>{" "}
+        <div className="rows">
+          <input
+            type="text"
+            placeholder="Insert description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Insert price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Insert quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
         </div>
-      <button onClick={AddProducts} className="btn btn-primary insertProduct">Insert</button>
-    </div>
-
-    <div className="StockUpdate container">
-      <header className="header">
-        <h2>Your Stock Tracker</h2>
-      </header>
-      <div className="stock-container">
-
-        <div className="stockdetail">
-         <form ><table className="stockTable">
-            <tbody>
-              <tr><th>ID</th> <th>catagory</th> <th>Image</th> <th>Title</th>  <th> Subtitle </th> <th>DESC</th><th>Price</th><th>Quantity</th><th></th></tr>
-              {products && products?.map((data) => {
-              <div> 
-              
-              </div> 
-                return (
-                <>  <tr>
-                    <td>
-                      <h5>{data.id}</h5>
-                    </td>
-                    <td>
-                      <h5>{data.category}</h5>
-                    </td>
-                    <td>
-                      <h5>{data.image}</h5>
-                    </td>
-                    <td>
-                      <h5>{data.title}</h5>
-                    </td>
-                    <td>
-                      <h5>{data.subtitle}</h5>
-                    </td>
-                    <td>
-                      <h5>{data.description}</h5>
-                    </td>
-                    <td>
-                      <h5>{data.price}</h5>
-                    </td>
-                    <td>
-                      <h5>{data.quantity}</h5>
-                    </td>
-                    <td className= "formAction">
-                      <button className="btn btn-primary" onClick={() => { deleteProduct(data.id) }} >Delete</button>
-                      <Link className="btn btn-primary"  to={`/update/${data.id}`} >Edit</Link>
-                    </td>
-                  </tr>
-                  </>
-                );
-              })}
-            </tbody>
-          </table></form> 
-        </div>
+        <button onClick={AddProducts} className="btn btn-primary insertProduct">
+          Insert
+        </button>
       </div>
-    </div>
-  </>
+      {loading ? (
+        " Loading............"
+      ) : (
+        <div className="StockUpdate container">
+          <header className="header">
+            <h2>Your Stock Tracker</h2>
+          </header>
+          <div className="stock-container">
+            <div className="stockdetail">
+              <form>
+                <table className="stockTable" id="FormData">
+                  <tr>
+                    <th>ID</th> <th>catagory</th> <th>Image</th> <th>Title</th>{" "}
+                    <th> Subtitle </th> <th>DESC</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th></th>
+                  </tr>
+                  <tbody>
+                    {products &&
+                      products?.map((data, key) => {
+                        return (
+                          <>
+                            <tr key={key}>
+                              <td>
+                                <h5>{data.id}</h5>
+                              </td>
+                              <td>
+                                <h5>{data.category}</h5>
+                              </td>
+                              <td>
+                                <h5>{data.image}</h5>
+                              </td>
+                              <td>
+                                <h5>{data.title}</h5>
+                              </td>
+                              <td>
+                                <h5>{data.subtitle}</h5>
+                              </td>
+                              <td>
+                                <h5>{data.description}</h5>
+                              </td>
+                              <td>
+                                <h5>{data.price}</h5>
+                              </td>
+                              <td>
+                                <h5>{data.quantity}</h5>
+                              </td>
+                              <td className="formAction">
+                                <button
+                                  className="btn btn-primary"
+                                  onClick={() => {
+                                    deleteProduct(data.id);
+                                  }}
+                                >
+                                  Delete
+                                </button>
+                                <Link
+                                  className="btn btn-primary"
+                                  to={`/update/${data.id}`}
+                                >
+                                  Edit
+                                </Link>
+                              </td>
+                            </tr>
+                          </>
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
