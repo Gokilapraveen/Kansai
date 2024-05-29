0282d137c066710f7aef6c61153131eca33bf305
GokilavaniPraveen <169182114+Gokilavanipraveen@users.noreply.github.com>
2024-05-29 19:46:20 +0530

    Update FilterDataCard.js

--- frontend/src/Component/FilterCard/FilterDataCard.js
+++ frontend/src/Component/FilterCard/FilterDataCard.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"; import "./FilterDataCard.css";
+import { useEffect, useState } from "react";
+import "./FilterDataCard.css";
 import Menu from "./Menu";
 
 function FilterDataCard() {
@@ -15,7 +16,9 @@ function FilterDataCard() {
   const removeCategory = (category) => {
     if (selectedCategories.includes(category)) {
       console.log(selectedCategories);
-      const removedList = selectedCategories.filter((item) => item !== category);
+      const removedList = selectedCategories.filter(
+        (item) => item !== category
+      );
       setSelectedCategories(removedList);
     }
   };
@@ -26,17 +29,31 @@ function FilterDataCard() {
     if (selectedCategories.length === 0) {
       setFilteredProductList(productList);
     } else {
-      setFilteredProductList(productList.filter((item) => selectedCategories.includes(item.category)));
+      setFilteredProductList(
+        productList.filter((item) => selectedCategories.includes(item.category))
+      );
     }
   }, [selectedCategories, productList]);
-  const allCategories = ["Laundry Image", "Floor Cleaning", "Glass Cleaning", "Air Freshner", "Dish Wash",];
+  const allCategories = [
+    "Laundry Image",
+    "Floor Cleaning",
+    "Glass Cleaning",
+    "Air Freshner",
+    "Dish Wash",
+  ];
   const getProducts = async () => {
     setLoading(true);
-    await fetch("http://localhost:3003/products").then((res) => res.json()).then((data) => {
-      setProductList(data);
-      setFilteredProductList(data);
-      setCategories(allCategories); // get the categories list  
-    }).catch((err) => alert(err)).finally(() => { setLoading(false); });
+    await fetch("http://localhost:3003/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProductList(data);
+        setFilteredProductList(data);
+        setCategories(allCategories); // get the categories list
+      })
+      .catch((err) => alert(err))
+      .finally(() => {
+        setLoading(false);
+      });
   };
   useEffect(() => {
     getProducts();
@@ -46,21 +63,36 @@ function FilterDataCard() {
       <h2 className="mx-3 ml-5 font-medium catagoryProduct ">Categories</h2>
       <div className="w-full h-[90%] rounded-md bg-white">
         <div className="relative w-full h-[15%] flex items-center overflow-x-auto productList productCatagoryDesktop">
-          {categories.map((category, index) => (<button key={index} onClick={() => {
-            if (selectedCategories.includes(category)) {
-              removeCategory(category);
-            }
-            else {
-              addCategory(category);
-            }
-          }}
-            className={`selectcat ${selectedCategories.includes(category) ? "btn btn-success" : " btn btn-primary btn-lg"} `}      >
-            {category}      </button>))}
-          <button onClick={() => resetCategory()
-
-          } className={`${selectedCategories.length > 0 ? "opacity-100" : "opacity-0 pointer-events-none"} btn btn-dark`}     >
-            Clear selection     </button>    </div>
-
+          {categories.map((category, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                if (selectedCategories.includes(category)) {
+                  removeCategory(category);
+                } else {
+                  addCategory(category);
+                }
+              }}
+              className={`selectcat ${
+                selectedCategories.includes(category)
+                  ? "btn btn-success"
+                  : " btn btn-primary btn-lg"
+              } `}
+            >
+              {category}{" "}
+            </button>
+          ))}
+          <button
+            onClick={() => resetCategory()}
+            className={`${
+              selectedCategories.length > 0
+                ? "opacity-100"
+                : "opacity-0 pointer-events-none"
+            } btn btn-dark`}
+          >
+            Clear selection{" "}
+          </button>{" "}
+        </div>
         {/* <div class="productCatagoryMobile"><div class="dropdown">
           <button onClick={(ele) => myFunction()} class="dropbtn" id="dropdownlabel">All</button>
           <div id="myDropdown" class="dropdown-content">
@@ -77,14 +109,12 @@ function FilterDataCard() {
 
           </div>
         </div></div> */}
-        {
-          <Menu menuItem={fileredProductList} loading={loading} />
-          }   </div>  
-          
-          <a href="/Products">Click here for Order</a>
-          </div>
+        {<Menu menuItem={fileredProductList} loading={loading} />}{" "}
+      </div>
+
+      <a href="/Products">Click here for Order</a>
+    </div>
   );
 }
-
 
 export default FilterDataCard;
